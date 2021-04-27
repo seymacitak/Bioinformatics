90dab92fd7ab8e90eae908f20bb9cc90942ffb6a
Seyma Citak <citak.seymaa@gmail.com>
2021-04-28 00:48:31 +0300

    validateSeq function added

---
+++ DnaTools.py
@@ -0,0 +1,9 @@
+Nucleotides = ["A","T","G","C"]
+
+def validateSeq(dna_seq):
+    tmpseq = dna_seq.upper()
+    for nuc in tmpseq:
+        if nuc not in Nucleotides:
+            return False
+        return tmpseq
+
