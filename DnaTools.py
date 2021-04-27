f48d3f47c9b05380985c4bff8b01fed0d634875f
Seyma Citak <citak.seymaa@gmail.com>
2021-04-28 02:17:42 +0300

    count nucleotid function added

--- DnaTools.py
+++ DnaTools.py
@@ -1,3 +1,5 @@
+import collections
+
 Nucleotides = ["A","T","G","C"]
 
 def validateSeq(dna_seq):
@@ -6,4 +8,16 @@ def validateSeq(dna_seq):
         if nuc not in Nucleotides:
             return False
         return tmpseq
+
+def countNucFrequency(seq):
+    tempFreqDict = {"A": 0, "T": 0, "G": 0, "C": 0}
+    for nuc in seq:
+        tempFreqDict[nuc] += 1
+    return tempFreqDict
+
+
+dnastring = "TAGCTACGATCGA"
+result = countNucFrequency(dnastring)
+print(result)
+
 
